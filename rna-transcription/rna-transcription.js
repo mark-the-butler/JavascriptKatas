a3e5379ba280d2dddf02ead351568b7406d845db
mdb513 <mark.d.butler@sherwin.com>
2017-07-03 11:58:58 -0400

    Wrote some ugly javascript that doesn't seem to work

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,12 +1,30 @@
 var DnaTranscriber = function() {
+    this.dnaDictionary = [
+            {key:"G",value:"C"},
+            {key:"C",value:"G"},
+            {key:"T",value:"A"},
+            {key:"A",value:"U"}
+        ];
+
+    this.rnaStrand = "";
 };
+
+DnaTranscriber.prototype.getDnaStrand = function(strand) {
+    this.dnaDictionary.forEach(function(element){
+        if (element.key === strand) {
+            this.rnaStrand += element.value;
+        }
+    });
+}
 
 DnaTranscriber.prototype.toRna = function(dnaStrand) {
-    if (dnaStrand === "G") {
-        return "C";
-    }
+    dnaStrand.split(" ").forEach(function(strand) {
+        getDnaStrand(this);
+    });
 
-    return "G";
+    return this.rnaStrand;
 };
+
+
 
 module.exports = DnaTranscriber;
